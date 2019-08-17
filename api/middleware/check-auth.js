742b5a9c2bbd83e592147c3af95d05ddddc31283
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-17 15:49:34 -0300

    add authorization with HTTP Post

--- api/middleware/check-auth.js
+++ api/middleware/check-auth.js
@@ -1,9 +1,12 @@
 const checkAuth = (request, response, next) => {
 
     const token = request.headers.authorization;
-    console.log('Token ===>'+ token);
-    next();
- 
-   };
-   
+    if (/ZG0xMjQ6YWx1bm9pbmF0ZWw=/.test(token)) {
+        next();
+      } else {
+        response.status(401).json({
+          error: 'Not authorized'
+        });
+      }
+     };
    module.exports = checkAuth;
