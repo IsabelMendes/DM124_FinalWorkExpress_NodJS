96233b841383177c65d5fc4047033c52be30ab07
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-17 13:33:50 -0300

    add tasks

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
+app.use(morgan('dev'));
 
 app.use((request, response, next) => {
-<<<<<<< HEAD
-    console.log(request.method, request.url);
- response.status(200).json({
-   message: 'It works baby!'
- });
-=======
-  console.log(request.method, request.url);
   response.status(200).json({
     message: 'It just works baby!!'
   })
->>>>>>> 529d410d18d66a186db72a038b380bcc38180bb8
+
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,12 +5,9 @@ const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 8080;
 
 http.createServer(app)
-<<<<<<< HEAD
- .listen(port, () => {
-   console.log(`Server up on http://${host}:${port}`);
- });
-=======
+
+
   .listen(port, () => {
     console.log(`Server up on http://${host}:${port}`);
   })
->>>>>>> 529d410d18d66a186db72a038b380bcc38180bb8
+
