43fe7a0db7360b98f266e5383dfa0e87287bfcc6
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-17 14:27:38 -0300

    Tasks with Patch and Delete

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -2,20 +2,34 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (request, response) => {
- response.status(200).json({
-   message: 'Handling POST requests to /tasks'
+ response.status(201).json({
+   message: 'The task has been created'
  });
 })
 router.get('/', (request, response) => {
  response.status(200).json({
-   message: 'Handling GET requests to /tasks'
+   message: 'Takes have been fetched'
  });
 })
-router.get('/:tasksId', (request, response) => {
+router.get('/:taskId', (request, response) => {
 const id = request.params.tasksId;
  response.status(200).json({
    message: `Task with ID = ${id} was fetched`
  });
 })
 
-module.exports = router;
+router.patch('/:taskId', (request, response) => {
+    const id = request.params.tasksId;
+     response.status(200).json({
+       message: `Task with ID = ${id} was updated`
+     });
+    })
+
+router.delete('/:taskId', (request, response) => {
+    const id = request.params.tasksId;
+        response.status(200).json({
+        message: `Task with ID = ${id} was deleted`
+    });
+})
+
+   module.exports = router;
