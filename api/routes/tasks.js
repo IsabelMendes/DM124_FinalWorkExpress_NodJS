acd31ba1016650997dae62361b2281b4160e20f7
IsabelMendes <isabelfrancinem@gmail.com>
2019-08-17 17:14:10 -0300

    db com Delete

--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -13,7 +13,7 @@ router.post('/', checkAuth, (request, response) => {
         description: request.body.description
     };
 
-    db [newTask.id] = newTask;
+    db[newTask.id] = newTask;
 
     response.status(201).json(newTask);
 })
@@ -22,33 +22,39 @@ router.get('/', (request, response) => {
     const toArray = key => db[key];
     const tasks = Object.keys(db).map(toArray);
     tasks && tasks.length
-    ? response.json(tasks)
-    : response.status(204).send();
+        ? response.json(tasks)
+        : response.status(204).send();
 });
 
 router.get('/:taskId', (request, response) => {
-const task = db [request.params.taskId];
-task
-    ? response.json(task)
-    : notFound(request, response);
- });
-
-router.patch('/:taskId',checkAuth, (request, response) => {
     const task = db[request.params.taskId];
+    task
+        ? response.json(task)
+        : notFound(request, response);
+});
+
+router.patch('/:taskId', checkAuth, (request, response) => {
+    const task = db[request.params.taskId];
+    const hasValue = request.body.done != null
     if (task) {
-      task.done = request.body.done || task.done;
+      task.done = hasValue ? request.body.done : task.done;
       task.description = request.body.description || task.description;
       response.json(task);
     } else {
       notFound(request, response);
     }
-   });   
-
-router.delete('/:taskId', checkAuth, (request, response) => {
-    const id = request.params.tasksId;
-        response.status(200).json({
-        message: `Task with ID = ${id} was deleted`
-    });
-})
-
-   module.exports = router;
+   });
+   
+   router.delete('/:taskId', checkAuth, (request, response) => {
+    const task = db[request.params.taskId];
+    if(task) {
+      delete db[task.id];
+      response.status(200).json({
+        message: 'Deleted'
+      });
+    } else {
+      notFound(request, response);
+    }
+   });
+   
+module.exports = router;
