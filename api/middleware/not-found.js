fb66e2bf58af1b27a9b7549bd24085fb83705540
IsabelMendes <isabelfrancinem@gmail.com>
2019-09-07 19:56:39 -0300

    DynamoDB_Final

--- api/middleware/not-found.js
+++ api/middleware/not-found.js
@@ -2,7 +2,7 @@ const notFound = (request, response, next) => {
     response.status(404).json({
       error: `Route to ${request.method} ${request.url} not found`
     });
-    next();
+    //next();
    };
    
    module.exports = notFound;
--- api/routes/entregas.js
+++ api/routes/entregas.js
@@ -3,20 +3,18 @@ const router = express.Router();
 const checkAuth = require('../middleware/check-auth');
 const notFound = require('../middleware/not-found');
 const uuidv1 = require('uuid/v1');
-
-
-var AWS = require("aws-sdk");
+const AWS = require("aws-sdk");
 
 AWS.config.update({
   region: "us-east-1",
   endpoint: "dynamodb.us-east-1.amazonaws.com"
 });
 
-var docClient = new AWS.DynamoDB.DocumentClient();
-var table = "entrega";
+const docClient = new AWS.DynamoDB.DocumentClient();
+const table = "entrega";
 
 router.post('/', checkAuth, (request, response) => {
-    var params = {
+    const params = {
         TableName:table,
         Item:{
             "id":uuidv1(),
@@ -36,27 +34,35 @@ router.post('/', checkAuth, (request, response) => {
             console.error("Unable to add item. Error JSON:", err);
             response.status(400).json(err);
         } else {
-            console.log("Added item:", data);
-            response.status(201).json(data);
+            console.log("Item added :", params.Item);
+            response.status(201).json(params.Item);
         }
     });
     
-})
-
-router.get('/', (request, response) => {
-
-
-
-    
-    const toArray = key => db[key];
-    const entregas = Object.keys(db).map(toArray);
-    entregas && entregas.length
-        ? response.json(entregas)
-        : response.status(204).send();
 });
 
+router.get('/', (request, response) => {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: table
+    };
+
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", err);
+            response.status(400).json(err);
+        } else {
+            console.log("Items:", data);
+            const { Items } = data;
+            response.status(201).json(Items);
+        }
+    });
+});
+
+
 router.get('/:entregaId', (request, response) => {
-    var params = {
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
         TableName: table,
         Key:{
             "id": request.params.entregaId
@@ -68,40 +74,66 @@ router.get('/:entregaId', (request, response) => {
             console.error("Unable to read item. Error JSON:", err);
             notFound(request,response);
         } else {
-            console.log("GetItem succeeded:", data);
-            response.json(data);
+            if (typeof data.Item !== 'undefined') {
+                console.log("GetItem succeeded:", data.Item);
+                response.json(data.Item);
+            } else {
+                notFound(request, response);
+            }
         }
     });
 
 });
 
-router.patch('/:entregaId', checkAuth, (request, response) => {
-    const entrega = db[request.params.entregaId];
-    const hasValue = request.body.recebedorComprador != null
-    if (entrega) {
-      entrega.idPedido = request.body.idPedido || entrega.idPedido;
-      entrega.idCliente = request.body.idCliente || entrega.idCliente;
-      entrega.nomeRecebedor = request.body.nomeRecebedor || entrega.nomeRecebedor;
-      entrega.cpfRecebedor = request.body.cpfRecebedor || entrega.cpfRecebedor;
-      entrega.dataEntrega = request.body.dataEntrega || entrega.dataEntrega;
-      entrega.horaEntrega = request.body.horaEntrega || entrega.horaEntrega;
-      entrega.localizaçãoGeografica = request.body.localizaçãoGeografica || entrega.localizaçãoGeografica;
-      response.json(entrega);
-    } else {
-      notFound(request, response);
-    }
-   });
+router.put('/:entregaId', checkAuth, (request, response) => {
+    const params = {
+        TableName:table,
+        Key: {
+            "id":request.params.entregaId
+        },
+        UpdateExpression: "set idPedido = :idPedido, idCliente = :idCliente, nomeRecebedor = :nomeRecebedor, cpfRecebedor = :cpfRecebedor, recebedorComprador = :recebedorComprador, dataEntrega = :dataEntrega, horaEntrega = :horaEntrega",
+        ExpressionAttributeValues:{
+            ":idPedido":request.body.idPedido,
+            ":idCliente":request.body.idCliente,
+            ":nomeRecebedor": request.body.nomeRecebedor,
+            ":cpfRecebedor": request.body.cpfRecebedor,
+            ":recebedorComprador":request.body.recebedorComprador,
+            ":dataEntrega":request.body.dataEntrega,
+            ":horaEntrega": request.body.horaEntrega
+        },
+        ReturnValues:"UPDATED_NEW"
+    };
+
+    docClient.update(params, function(err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", err);
+            response.status(400).json(err);
+        } else {
+            console.log("Item updated:", params.Item);
+            response.status(200).json(request.body);
+        }
+    });
+    
+});
    
    router.delete('/:entregaId', checkAuth, (request, response) => {
-    const entrega = db[request.params.entregaId];
-    if(entrega) {
-      delete db[entrega.id];
-      response.status(200).json({
-        message: 'Deleted'
-      });
-    } else {
-      notFound(request, response);
-    }
+    const docClient = new AWS.DynamoDB.DocumentClient();
+    const params = {
+        TableName: table,
+        Key:{
+            "id": request.params.entregaId
+        }
+    };
+
+    docClient.delete(params, function(err, data) {
+        if (err) {
+            console.error("Unable to delete item. Error JSON:", err);
+            notFound(request,response);
+        } else {
+            console.log("Data:", data);
+            response.status(200).json(data);
+        }
+    });
    });
    
 module.exports = router;
