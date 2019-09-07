ec5d3266e6b3a6d811dfffa60669004bb88e8bdf
IsabelMendes <isabelfrancinem@gmail.com>
2019-09-07 15:23:00 -0300

    endpointHealth

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use('/api/tasks', require('./api/routes/tasks'));
 app.use('/api/products', require('./api/routes/products'));
 app.use('/api/entregas', require('./api/routes/entregas'));
+app.use('/api/health', require('./api/routes/health'));
 app.use(require('./api/middleware/not-found'));
 
 module.exports = app;
